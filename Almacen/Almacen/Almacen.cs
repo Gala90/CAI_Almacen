e2145b76129f53460442dc27ba22d455e1261e60
Gala <galamagdalena@hotmail.com>
2020-03-31 00:33:59 -0300

    Inventario

---
+++ Almacen/Almacen/Almacen.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+ namespace Almacen
+   {
+        public class Almacen
+        {
+            private string nombre;
+            private string direccion;
+            private Inventario inv;
+            private int cantClientesAtendidos;
+            private List<String> empleados;
+
+
+
+
+            public Almacen(string nombre, string direccion)
+            {
+                this.nombre = nombre;
+                this.direccion = direccion;
+                cantClientesAtendidos = 0;
+                empleados = new List<string>();
+            }
+
+            public String Nombre
+            {
+                get
+                {
+                    return this.nombre;
+                }
+                set { this.nombre = value; }
+            }
+
+            public String Direccion
+            {
+                get
+                {
+                    return this.direccion;
+                }
+                set { this.direccion = value; }
+            }
+
+
+            public void mostrarAlmacen()
+            {
+                Console.WriteLine("Nombre: " + Nombre + " Direccion: " + Direccion);
+            }
+
+
+        }
+    }
+
+
+
---
+++ Almacen/Almacen/Inventario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Almacen
+{
+    public class Inventario
+    {
+        private int codigo;
+        private string producto;
+        private string marca;
+        private int cant;
+        private double precio;
+
+
+
+
+    public Inventario (int codigo, string producto, string marca, int cant, double precio)
+        {
+            this.codigo = codigo;
+            this.producto = producto;
+            this.marca = marca;
+            this.cant = cant;
+            this.precio = precio;
+
+        }
+
+    }
+}
